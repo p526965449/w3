97f2a76e8353a84afddf96b6612f6c9d5b9fc41c
Praveen Renganathan <praveenmanickam.renganathan@kellogg.northwestern.edu>
2024-02-17 21:58:26 +0000

    Update1

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+     get "/travel", :controller => "travel", :action => "index"
   # Defines the root path route ("/")
   # get("/", { :controller => "articles", :action => "index" })
 end
